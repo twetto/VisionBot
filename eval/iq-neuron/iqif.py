5d7e8218d1d0c57516715bb1dc151d448190403c
twetto <franky85@hotmail.com.tw>
2020-07-10 10:19:29 +0800

    Add pseudo lif lib to iqif.py (commented out)

--- eval/iq-neuron/iqif.py
+++ eval/iq-neuron/iqif.py
@@ -7,16 +7,20 @@ if platform.system() == "Windows":
     buildFolder = Path("iq-neuron/out/build/x64-Debug")
     libiqPath = str(buildFolder / "iq-network.dll")
     libizPath = str(buildFolder / "iz-network.dll")
+    #liblifPath = str(buildFolder / "lif-network.dll")
 else:
     buildFolder = Path("iq-neuron/build/")
     libiqPath = buildFolder / "libiq-network.so"
     libizPath = buildFolder / "libiz-network.so"
+    #liblifPath = buildFolder / "liblif-network.so"
 
 try:
     libiq = ctypes.CDLL(libiqPath)
     libiz = ctypes.CDLL(libizPath)
+    #liblif = ctypes.CDLL(liblifPath)
 except OSError:
     print("Unable to load iq/iz shared library.")
+    #print("Unable to load iq/iz/lif shared library.")
     sys.exit()
 
 
@@ -117,4 +121,53 @@ class iznet(object):
 
     def spike_rate(self, neuron_index):
         return libiz.iz_network_spike_rate(self.obj, neuron_index)
+'''
+class lifnet(object):
+    def __init__(self):
+        liblif.lif_network_new.argtypes = None
+        liblif.lif_network_new.restype = ctypes.c_void_p
 
+        liblif.lif_network_num_neurons.argtypes = [ctypes.c_void_p]
+        liblif.lif_network_num_neurons.restype = ctypes.c_int
+
+        liblif.lif_network_send_synapse.argtypes = [ctypes.c_void_p]
+        liblif.lif_network_send_synapse.restype = ctypes.c_void_p
+
+        liblif.lif_network_set_biascurrent.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.c_int]
+        liblif.lif_network_set_biascurrent.restype = ctypes.c_void_p
+
+        liblif.lif_network_potential.argtypes = [ctypes.c_void_p, ctypes.c_int]
+        liblif.lif_network_potential.restype = ctypes.c_float
+
+        liblif.lif_network_adaptive_term.argtypes = [ctypes.c_void_p, ctypes.c_int]
+        liblif.lif_network_adaptive_term.restype = ctypes.c_float
+
+        liblif.lif_network_spike_count.argtypes = [ctypes.c_void_p, ctypes.c_int]
+        liblif.lif_network_spike_count.restype = ctypes.c_int
+
+        liblif.lif_network_spike_rate.argtypes = [ctypes.c_void_p, ctypes.c_int]
+        liblif.lif_network_spike_rate.restype = ctypes.c_float
+
+        self.obj = liblif.lif_network_new()
+
+    def num_neurons(self):
+        return liblif.lif_network_num_neurons(self.obj)
+
+    def send_synapse(self):
+        return liblif.lif_network_send_synapse(self.obj)
+
+    def set_biascurrent(self, neuron_index, biascurrent):
+        return liblif.lif_network_set_biascurrent(self.obj, neuron_index, biascurrent)
+
+    def potential(self, neuron_index):
+        return liblif.lif_network_potential(self.obj, neuron_index)
+
+    def adaptive_term(self, neuron_index):
+        return liblif.lif_network_adaptive_term(self.obj, neuron_index)
+
+    def spike_count(self, neuron_index):
+        return liblif.lif_network_spike_count(self.obj, neuron_index)
+
+    def spike_rate(self, neuron_index):
+        return liblif.lif_network_spike_rate(self.obj, neuron_index)
+'''
